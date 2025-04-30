f6ba5004040561b581d2cfea619d1048902c640d
jaeyo03 <toyazx@naver.com>
2025-04-30 16:32:16 +0900

    initial commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,31 @@
+import type { Metadata } from "next";
+import localFont from "next/font/local";
+import "./globals.css";
+
+const pretendard = localFont({
+  src: "../fonts/PretendardVariable.woff2",
+  display: "swap",
+  weight: "45 920",
+  variable: "--font-pretendard",
+});
+
+export const metadata: Metadata = {
+  title: "팔구삼 893",
+  description: "중고 경매 사이트",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="ko">
+      <body
+        className={`${pretendard.variable} font-pretendard`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}

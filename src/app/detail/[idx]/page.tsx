ebd73718f36e63de996e949db1e1a091b6864e2e
msckr <mschoyb@gmail.com>
2025-05-02 17:57:26 +0900

    Feat: Component in detailPage

--- src/app/detail/[idx]/page.tsx
+++ src/app/detail/[idx]/page.tsx
@@ -1,5 +1,8 @@
 import ImageSlider from "@/components/detail/ImageSlider";
 import ProductInfo from "@/components/detail/ProductInfo";
+import BidHistory from "@/components/detail/BidHistory";
+import GoodsInfo from "@/components/detail/GoodsInfo"
+import ProductCard from "@/components/detail/ProductCard";
 
 interface DetailPageProps{
     params: {
@@ -7,27 +10,49 @@ interface DetailPageProps{
     };
 }
 
+
 export default function DetailPage({params} : DetailPageProps) {
   const itemId = params.idx
+  {/*api 호출 필요*/}
+  const product = "해당 상품은 아디다스 신발입니다.\n 사주세요.";
+  const relatedProducts = [
+    {
+      imageUrl: "/images/adidas_shoe.jpg",
+      title: "아디다스 삼바 블랙 팝니다",
+      status: "yet",
+      startTime: "2025-04-20",
+      endTime: "2025-04-26",
+      currentPrice: 89000,
+      bidderCount: 13,
+      scrapCount: 10,
+    },
+    // 다른 상품도 추가 가능
+  ];
   return(
-    <div style={{ display: 'flex', justifyContent: 'space-between', padding: '20px' }}>
-      <div style={{ flex: 1, marginRight: '20px' }}>
-        <ImageSlider />
-      </div>
-    
-      <div style={{ flex: 1, maxWidth: '620px' }}>
-        <ProductInfo />
-      </div>
+    <><div style={{ display: 'flex', justifyContent: 'space-between', padding: '20px' }}>
+        <div style={{ flex: 1, marginRight: '20px' }}>
+          <ImageSlider />
+          <GoodsInfo description={product} />
+        </div>
+
+        <div style={{ flex: 1, maxWidth: '620px' }}>
+          <ProductInfo />
+          <BidHistory />
+        </div>
+
     </div>
+    <hr />
+    <div style={{padding: '20px'}}>
+    <h2 className="pl-4 mb-2 text-xl font-bold">관련 상품</h2>
+      <div className="flex gap-6 pl-4 overflow-x-auto scrollbar-hide">
+          {relatedProducts.map((item, index) => (
+            <div className="min-w-[231px]" key={index}>
+              <ProductCard {...item} />
+            </div>
+          ))}
+      </div>
+      </div>
+    </>
+    
     );
 }
-
-
-//상품 이미지 컴포넌트 -> 이미지, 경매 상태, 북마크(찜)
-//이미지 슬라이딩 표시 컴포넌트
-//상세 설명 -> 카테고리, 상품 상태, 설명
-//경매 정보(경매 제목(string),판매자 이메일(string), 현재가, 남은 시간, 입찰 횟수, 찜(관심도)수, 입찰자 수,입찰 금액 입력, 입찰하기(결제하기-> 경매 종료시, 낙찰자인 경우))
-//진행 중 -> 입찰 한 경우 -> 취소 버튼 생성
-//입찰 내역
-//입찰 취소 내역
-//연관 상품

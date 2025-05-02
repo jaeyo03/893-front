9a0faade383026e681caf6c07bed452a47928708
msckr <mschoyb@gmail.com>
2025-05-02 09:17:37 +0900

    feat: add ImageSlider

---
+++ src/components/detail/ImageSlider.tsx
@@ -0,0 +1,75 @@
+'use client'
+
+import { useState } from "react"
+import { ChevronLeft, ChevronRight } from 'lucide-react';
+
+import AuctionItemCard from "./AuctionItemCard"
+
+export default function ImageSlider(){
+  const images =['/images/adidas_shoe.jpg',
+    '/images/converse.avif',
+    '/images/nike_shoe.jpg',
+  ];
+
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const goToNext = () => {
+    setCurrentIndex((prev) => (prev + 1) % images.length);
+  };
+
+  const goToPrev = () => {
+    setCurrentIndex((prev) => (prev - 1 + images.length) % images.length);
+  };
+  return (
+    <div className="relative flex flex-col">
+      <div className="relative w-[600px] h-[600px] group">
+        <AuctionItemCard imageUrl={images[currentIndex]} label="진행중"/>
+          {/* <button
+          className="absolute z-10 p-8 text-xl text-white transform -translate-y-1/2 rounded-full top-1/2 left-10 bg-black/10 hover:bg-black/70"
+          onClick={goToPrev}
+        >
+          <ChevronLeft size={32}/>
+        </button>
+        <button
+          className="absolute z-10 p-8 text-xl text-white transform -translate-y-1/2 rounded-full top-1/2 right-1 bg-black/10 hover:bg-black/70"
+          onClick={goToNext}
+        >
+          <ChevronRight size={32}/>
+        </button> */}
+        <div
+        
+        className="absolute top-0 left-0 h-full w-[150px] z-20 cursor-pointer flex items-center justify-start bg-transparent"
+      >
+        <ChevronLeft
+          size={40}
+          onClick={goToPrev}
+          className="absolute p-1 text-white transition-opacity duration-200 -translate-y-1/2 rounded-full opacity-0 top-1/2 left-12 bg-black/50 group-hover:opacity-100"
+        />
+      </div>
+      <div
+        
+        className="absolute top-0 right-0 h-full w-[150px] z-20 cursor-pointer flex items-center justify-end bg-transparent"
+      >
+        <ChevronRight
+          size={40}
+          onClick={goToNext}
+          className="absolute p-1 text-white transition-opacity duration-200 -translate-y-1/2 rounded-full opacity-0 top-1/2 right-4 bg-black/50 group-hover:opacity-100"
+        />
+      </div>
+      <div className="absolute z-30 flex space-x-2 -translate-x-1/2 bottom-4 left-1/2">
+        {images.map((_, index) => (
+            <div
+              key={index}
+              onClick={() => setCurrentIndex(index)}
+              className={`w-3 h-3 rounded-full cursor-pointer ${
+                index === currentIndex ? 'bg-imageindex' : 'bg-rightgray'
+              }`}
+            />
+        ))}
+      </div>
+      </div>
+      
+
+      
+    </div>
+  );
+}

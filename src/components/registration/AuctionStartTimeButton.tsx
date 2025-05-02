526f86a98874c8c1b5e1d092bde7210c6550cf59
LeeJaeHo <l2281@naver.com>
2025-05-02 19:22:05 +0900

    feat: add AuctionStartTimeButton components

---
+++ src/components/registration/AuctionStartTimeButton.tsx
@@ -0,0 +1,9 @@
+import { Button } from "../ui/button";
+
+export default function CategorySelect() {
+  return (
+    <Button className="w-[560px] h-[50px] bg-white border border-registerline text-resgistertext text-[16px] font-thin rounded-[6px] px-[16px] shadow-none text-left hover:bg-divider">
+      경매 시작 시간 (최대 24시간)
+    </Button>
+  );
+}

db9ad900b554d5ce8b4747d0b121fddec584c6d5
msckr <mschoyb@gmail.com>
2025-05-13 22:07:46 +0900

    feat: fix ProductCard수정완료

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,9 @@ import SpinningWord from "@/components/atoms/SpinningWord";
 import CategorySearchSection from "@/components/molecules/CategorySearchSection";
 import SearchInput from "@/components/templates/SearchInput";
 
-import ProductCard from "@/components/detail/Product/ProductCard";
+import AuctionCard from "@/components/detail/Product/ProductCard";
+import type { Product } from "@/types/productData";
 
-import ProductCard from "@/components/detail/ProductCard";
 import { Metadata } from "next";
 // async function getProducts() {
 //   const res = await fetch('http://localhost:3000/mocks/products.json'); // TODO 추후 백엔드 URI로 변경
@@ -25,6 +25,34 @@ export const metadata: Metadata = {
 export default async function Home() {
   // const products = await getProducts(); // TODO 추후 상품 섹션과 연결
 
+  const mockProduct:Product = {
+    auctionId: 1,
+    title: "상품 1",
+    description: "설명 1",
+    sellerEmailMasked: "example1@*****.com",
+    status: "yet",
+    itemCondition: "like_new",
+    basePrice: 10000,
+    isScrap: false,
+    scrapCount: 3,
+    isSeller: false,
+    category: {
+      id: 1,
+      mainCategory: "전자기기",
+      subCategory: "휴대폰",
+      detailCategory: "스마트폰",
+    },
+    startTime: "2024-01-01",
+    endTime: "2024-01-10",
+    mainImage: {
+      originalName: "sample.jpg",
+      storeName: "sample_1.jpg",
+      url: "https://via.placeholder.com/150",
+      imageSeq: 1,
+    },
+    images: [],
+  };
+  
   return (
     <>
       <div className="grid justify-center items-center gap-4 w-full">
@@ -38,17 +66,7 @@ export default async function Home() {
         <CategorySearchSection/>
       </div>
       <div className="grid grid-cols-5 gap-4 mt-8">
-        <ProductCard
-          imageUrl="https://via.placeholder.com/150"
-          title="상품 1"
-          status="진행중"
-          startTime="2024-01-01"
-          endTime="2024-01-10"
-          currentPrice={10000}
-          bidderCount={10}
-          scrapCount={10}
-          isScrapped={false}
-        />
+        <AuctionCard product={mockProduct}/>
       </div>
     </>
   );

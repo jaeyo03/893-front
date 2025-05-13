38d9290f185b70801757d02f92c2c03c3eee6794
msckr <mschoyb@gmail.com>
2025-05-13 17:30:57 +0900

    feat: create interface file

--- src/types/productData.ts
+++ src/types/productData.ts
@@ -1,17 +1,26 @@
-export interface RelatedProduct {
-  imageUrl: string;
-  sellerEmail: string;
+export interface Product {
+  auctionId: number;
   title: string;
-  status: 'yet' | 'done' | 'in-progress';
+  description: string;
+  sellerEmailMasked: string;
+  status: Status;
+  itemCondition: ItemCondition;
+  basePrice: number;
+  isScrap: boolean;
+  scrapCount: number;
+  isSeller: boolean;
+  category: Category;
   startTime: string;
   endTime: string;
-  currentPrice: number;
-  startPrice:number
-  bidCount: number;
-  bidderCount: number;
-  scrapCount: number;
-  category: Category;
-  itemId: number;
+  mainImage: Image;
+  images: Image[];
+}
+
+export interface Image{
+  originalName: string;
+  storeName: string;
+  url: string;
+  imageSeq:number;
 }
 
 export interface Category{
@@ -21,48 +30,29 @@ export interface Category{
   detailCategory:string;
 }
 
-export const getProductDescription = (itemId: number): string => {
-  // 추후 API 연동 시 이 부분을 대체
-  return "해당 상품은 아디다스 신발입니다.\n 사주세요.";
-};
+export type ItemCondition = 'brand_new' | 'like_new' | 'gently_used' | 'heavily_used' | 'damaged';
 
-export const getRelatedProducts = (itemId: number): RelatedProduct[] => {
-  return [
-    {
-      imageUrl: "/images/adidas_shoe.jpg",
-      title: "아디다스 삼바 블랙 팝니다",
-      status: "yet",
-      startTime: "2025-04-20",
-      endTime: "2025-05-13 10:00:00",
-      currentPrice: 30000,
-      startPrice:25000,
-      bidCount: 3,
-      bidderCount:2,
-      scrapCount: 10,
-      category:
-        {
-          id : 1,
-          mainCategory: "의류",
-          subCategory: "신발",
-          detailCategory: "아디다스",
-        }
-      ,
-      itemId: itemId,
-      sellerEmail: "g***@gmail.com"
-    },
-    // 다른 상품들 추가 가능
-  ];
-};
+export type Status = 'pending' | 'completed' | 'yet';
 
 export interface Bid {
-  rank: number;
-  email: string;
-  amount: number;
-  time: string;
+  bidId: number;
+  bidderEmail: string;
+  bidPrice: number;
+  createdAt: string;
+  updatedAt: string | null;
+}
+
+export interface AuctionBidData {
+  auctionId: number;
+  totalBid: number;
+  totalBidder: number;
+  bids: Bid[];
+  cancelledBids: Bid[];
 }
 
 export interface AuctionStatsProps {
-  relatedProducts: RelatedProduct;
+  product: Product;
+  auctionBidData: AuctionBidData;
   isBookmarked: boolean;
   bookmarkCount: number;
   onBookmarkToggle: () => void;

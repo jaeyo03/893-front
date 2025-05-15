9618d17a4da338037e2ed33893368a28a7a11451
msckr <mschoyb@gmail.com>
2025-05-15 16:12:30 +0900

    feat:  api인터페이스 추가

--- src/types/userData.ts
+++ src/types/userData.ts
@@ -15,3 +15,45 @@ export interface DeliveryAddress {
   zipCode: string,
   isDefault: boolean,
 }
+export type Status = 'pending' | 'active' | 'completed' | 'cancelled';
+
+export interface MyBidProduct {
+  bidId: number,
+  isWinning: boolean,
+  bidHighestPrice: number,
+  userPrice: number,
+  title: string,
+  endTime: string,
+  startTime: string,
+  status: Status,
+  auctionId: number,
+  mainImageUrl: string,
+}
+
+export interface MyAuctionsProduct {
+  auctionId: number,
+  bidHighestPrice: number,
+  title: string,
+  endTime: string,
+  startTime: string,
+  status: Status,
+  mainImageUrl: string,
+}
+
+export interface MyScrapsProduct {
+  auctionId: number,
+  bidHighestPrice: number,
+  title: string,
+  endTime: string,
+  startTime: string,
+  status: Status,
+  mainImageUrl: string,
+}
+
+export interface MyPaymentsProduct {
+  auctionId: number,
+  orderNumber: string,
+  mainImageUrl: string,
+  finalPrice: number,
+  title: string,
+}

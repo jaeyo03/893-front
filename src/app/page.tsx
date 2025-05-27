0c4a0fdf7a0193b7259fdd8537e38970a1a27f1d
LeeJaeHo <l2281@naver.com>
2025-05-27 18:03:06 +0900

    feat: add recentauctionList component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,30 +1,33 @@
 import HomeTitle from "@/components/molecules/HomeTitle";
 import SpinningWord from "@/components/atoms/SpinningWord";
 import SearchInput from "@/components/templates/SearchBox";
-import {Metadata} from "next";
+import { Metadata } from "next";
 import QueryProvider from "@/components/QueryProvider";
 import { cookies } from "next/headers";
-
+import RecentAuctionList from "@/components/home/RecentAuctionList";
 export const metadata: Metadata = {
   title: "중고 경매 플랫폼 팔구삼 893",
   description: "중고 상품을 경매로 사고 팔 수 있는 팔구삼 893",
-}
+};
 
 export default async function Home() {
   const cookieStore = cookies();
-	const accessToken = cookieStore.get('accessToken')?.value
-  
+  const accessToken = cookieStore.get("accessToken")?.value;
+
   return (
     <>
-      <div className="grid justify-center items-center gap-6 w-full mt-6">
+      <div className="grid w-full gap-6 mt-6">
         <HomeTitle>
-          <SpinningWord/>
+          <SpinningWord />
           <span>&nbsp;경매에 참여해보세요</span>
         </HomeTitle>
         <div className="flex justify-center w-full">
           <QueryProvider>
-            <SearchInput isLogin={accessToken ? true : false}/>
+            <SearchInput isLogin={accessToken ? true : false} />
           </QueryProvider>
+        </div>
+        <div className="flex justify-start w-[800px] h-[400px]">
+          <RecentAuctionList />
         </div>
       </div>
     </>

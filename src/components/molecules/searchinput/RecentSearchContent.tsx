c6abd1931234f8401ebdffde19055bb5f1778a55
jaeyo03 <toyazx@naver.com>
2025-06-04 15:26:04 +0900

    fix: add aria-label & role

--- src/components/molecules/searchinput/RecentSearchContent.tsx
+++ src/components/molecules/searchinput/RecentSearchContent.tsx
@@ -13,7 +13,7 @@ interface RecentSearchContentProps {
 
 export default function RecentSearchContent({ handleClickWordButton, recentSearches, handleDeleteSearch, popularSearches, handleCloseSearchContent } : RecentSearchContentProps) {
   return (
-    <div className="absolute z-10 rounded-b-[12px] w-full -ml-[1.5px] bg-white grid gap-4 border-l-[1.5px] border-r-[1.5px] border-b-[1.5px] border-main" style={{ width: 'calc(100% + 3px)' }}>
+    <div className="absolute z-30 rounded-b-[12px] w-full -ml-[1.5px] bg-white grid gap-4 border-l-[1.5px] border-r-[1.5px] border-b-[1.5px] border-main" style={{ width: 'calc(100% + 3px)' }}>
       <div className="border-t border-t-[#E5E9EC]">
         <div className="pr-4 pl-4 pt-4 pb-2 text-[#373737] flex text-sm items-center justify-between">
           <div>최근 검색어</div>
@@ -49,7 +49,7 @@ export default function RecentSearchContent({ handleClickWordButton, recentSearc
         </div>
       </div>
       <div className="pl-4 pr-4 pt-2 pb-2 border-t-[1px] border-[#EFF2F4] flex justify-end">
-        <button type="button" onClick={handleCloseSearchContent} className="text-[#898989]">
+        <button aria-label="최근 검색 창 닫기" type="button" onClick={handleCloseSearchContent} className="text-[#898989]">
           닫기
         </button>
       </div>

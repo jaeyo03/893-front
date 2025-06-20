9af193c219fde9f9c03644208444dbffd40f7e03
LeeJaeHo <l2281@naver.com>
2025-06-20 14:08:50 +0900

    feat: add DashboardStats and DashboardStatsSkeleton components

---
+++ src/components/home/DashboardStats.tsx
@@ -0,0 +1,42 @@
+// components/home/DashboardStats.tsx
+
+import type { DashboardStatsResponse } from "@/lib/api/home";
+import { getDashboardStats } from "@/lib/api/home";
+interface StatCardProps {
+  label: string;
+  value: number;
+}
+
+function StatCard({ label, value }: StatCardProps) {
+  return (
+    <div className="border rounded-sm p-4 w-[246px] h-[100px]">
+      <p className="text-sm text-main font-semibold">{label}</p>
+      <p className="text-2xl font-bold mt-4">{value?.toLocaleString()}</p>
+    </div>
+  );
+}
+
+export default async function DashboardStats() {
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_API_URL}/api/home/stats`,
+    { cache: "no-store" }
+  );
+  const { data: dashboardStats }: { data: DashboardStatsResponse } = await getDashboardStats();
+
+  const statDataList: StatCardProps[] = [
+    { label: "현재 이용자 수", value: dashboardStats?.totalUserCount },
+    { label: "현재 등록된 경매 수", value: dashboardStats?.totalAuctionCount },
+    {
+      label: "현재 진행중인 경매 수",
+      value: dashboardStats?.activeAuctionCount,
+    },
+  ];
+
+  return (
+    <div className="flex flex-col sm:flex-row gap-4">
+      {statDataList.map((stat, index) => (
+        <StatCard key={index} label={stat.label} value={stat.value} />
+      ))}
+    </div>
+  );
+}
